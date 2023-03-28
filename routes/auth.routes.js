2b93f2e76a5bdc5efb5df3b345d5cda2d0405e1f
saracsilva <saracarolinasilva@gmail.com>
2023-03-28 16:15:15 +0200

    created signup route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,65 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User.model");
+const saltRounds = 10;
 
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
+router.post("/signup", (req, res, next) => {
+  const { email, password, firstName, lastName } = req.body;
+
+  // Check if email or password or name are provided as empty string
+  if (email === "" || password === "" || firstName === "" || lastName === "") {
+    res.status(400).json({ message: "Provide email, password and name" });
+    return;
+  }
+  // Use regex to validate the email format
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+  if (!emailRegex.test(email)) {
+    res.status(400).json({ message: "Provide a valid email address." });
+    return;
+  }
+  // Use regex to validate the password format
+  const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!passwordRegex.test(password)) {
+    res.status(400).json({
+      message:
+        "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.",
+    });
+    return;
+  }
+  // Check the users collection if a user with the same email already exists
+  User.findOne({ email })
+    .then((foundUser) => {
+      // If the user with the same email already exists, send an error response
+      if (foundUser) {
+        res.status(400).json({ message: "User already exists" });
+        return;
+      }
+
+      // If email is unique, proceed to hash the password
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hashedPassword = bcrypt.hashSync(password, salt);
+
+      // Create the new user in the database
+      return User.create({
+        email,
+        password: hashedPassword,
+        firstName,
+        lastName,
+      });
+    })
+    .then((createdUser) => {
+      // Deconstruct the newly created user object to omit the password
+      const { email, firstName, lastName, _id } = createdUser;
+
+      // Create a new object that doesn't expose the password
+      const user = { email, firstName, lastName, _id };
+
+      // Send a json response containing the user object
+      res.status(201).json({ user: user });
+    })
+    .catch((err) => next(err));
 });
+
 module.exports = router;
