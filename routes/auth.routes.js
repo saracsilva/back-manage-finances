87f6dbcb1a729e992af52a8796f6b088709c5749
saracsilva <saracarolinasilva@gmail.com>
2023-04-05 12:54:44 +0200

    created middleware and verify route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User.model");
 const saltRounds = 10;
+const { isAuthenticated } = require("./../middleware/jwt.middleware.js");
 
 router.post("/signup", (req, res, next) => {
   const { email, password, firstName, lastName } = req.body;
@@ -102,5 +103,13 @@ router.post("/login", (req, res, next) => {
     })
     .catch((err) => res.status(500).json({ message: "Internal Server Error" }));
 });
+router.get("/verify", isAuthenticated, (req, res, next) => {
+  // If JWT token is valid the payload gets decoded by the
+  // isAuthenticated middleware and made available on `req.payload`
+  console.log(`req.payload`, req.payload);
 
+  // Send back the object with user data
+  // previously set as the token payload
+  res.status(200).json(req.payload);
+});
 module.exports = router;
